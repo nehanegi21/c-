4e8eed83b30dc49c48bb2284808ba84dbf1ef098
nehanegi21 <113226096+nehanegi21@users.noreply.github.com>
2022-09-10 11:07:25 +0530

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class demo
+{
+	int a,b;
+	
+	public:
+		demo()
+		{
+		  a=10,b=20;
+	       	cout<<a<<b;
+		}
+	};
+	
+	int main()
+	{
+		demo ob;
+		
+		return 0;
+	
+}
